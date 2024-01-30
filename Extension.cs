003387458aea926b6efe3c78ad68cee879af4dfc
BankApp <https://github.com/@etb15112003>
2024-01-30 12:18:53 +0500

    Initial commit

---
+++ Extension.cs
@@ -0,0 +1,47 @@
+﻿namespace Extensions;
+
+public static class Extension
+{
+    public static T[] Remove<T>(this T[] res,  int a)
+    {
+        T[] arr = new T[res.Length-1];
+
+        for (int i = 0; i <res.Length; i++)
+        {
+            if (i < (a - 1) )
+            {
+                 arr[i]=res[i];
+            }
+            else if (i >(a - 1))
+            {
+               arr[i-1] = res[i];
+            }
+           
+        }
+        return arr;
+    }
+
+
+    public static T[] Add<T>(this T[] res, int a,T t)
+    {
+        T[] arr = new T[res.Length + 1];
+        for (int i = 0; i < res.Length+1; i++)
+        {
+            if (i < (a - 1))
+            {
+                arr[i] = res[i];
+            }
+            else if (i == (a - 1))
+            {
+                arr[i] = t;
+            }
+            else if (i > (a - 1))
+            {
+                arr[i] = res[i+1];
+                
+            }
+        }
+
+        return arr;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Threading.Channels;
+using Extensions;
+
+
+int[] ar=new []{4,5,6,8,9,6,9};
+// Console.Write("Qaysi indexga o'chirasiz:  ");
+// int t=int.Parse(Console.ReadLine()); 
+// var res=ar.Remove(t);
+// foreach (var i in res)
+// {
+//    // Console.Write(i+"   ");
+// }
+
+Console.Write("Qaysi element qo'shasiz:  ");
+int t = int.Parse(Console.ReadLine());
+
+Console.Write("Qaysi indexga qo'shasiz:   ");
+int a = int.Parse(Console.ReadLine());
+
+var res=ar.Add(a, t);
+
+foreach (var i in res)
+{
+    Console.Write(i+" ");
+}
+
+
+
